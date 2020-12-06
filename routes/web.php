ed59a8261a49012afa270fc836b1ed02b0cfded4
Fardin Ahmed Niloy <niloy98.it@gmail.com>
2020-12-07 02:51:29 +0600

    added route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+	echo "index page";
 });
+
+
+Route::get('/login', 'App\Http\Controllers\loginController@index');
+Route::post('/login', 'App\Http\Controllers\loginController@verify');
+
+Route::get('/home', 'App\Http\Controllers\homeController@index');
+Route::get('/logout', 'App\Http\Controllers\logoutController@index');
+
+Route::get('/create', 'App\Http\Controllers\homeController@create');
+Route::post('/create', 'App\Http\Controllers\homeController@insert');
+
+Route::get('/stdlist', 'App\Http\Controllers\homeController@stdlist');
+Route::get('/details/{id}', 'App\Http\Controllers\homeController@details');
+
+Route::get('/edit/{id}', 'App\Http\Controllers\homeController@edit');
+Route::post('/edit/{id}', 'App\Http\Controllers\homeController@update');
+
+Route::get('/delete/{id}', 'App\Http\Controllers\homeController@delete');
+Route::post('/delete/{id}', 'App\Http\Controllers\homeController@destroy');
+
