06d004bd891ba471991411839e9e17f62afd85ad
Fardin Ahmed Niloy <niloy98.it@gmail.com>
2020-12-23 12:37:47 +0600

    added web.php and logout controller

--- Final_Lab_Exam/routes/web.php
+++ Final_Lab_Exam/routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('users.login');
+// });
+Route::get('/', 'UsersController@login');
+Route::post('/', 'UsersController@verifyLogin');
+Route::get('/login', 'UsersController@login');
+Route::post('/login', 'UsersController@verifyLogin');
+Route::get('/logout', 'LogoutController@index');
+
+Route::group(['middleware'=>['profile']], function(){
+    Route::get('/adminHome', 'UsersController@adminHome')->name('users.admin.home');
+    Route::get('/usersCreate', 'UsersController@create')->name('users.admin.create');
+    Route::post('/usersCreate', 'UsersController@store')->name('users.admin.create');
+    Route::get('/userAdminEdit/{id}', 'UsersController@edit')->name('users.admin.edit');
+    Route::post('/userAdminEdit/{id}', 'UsersController@update')->name('users.admin.edit');
+    Route::get('/userAdminDelete/{id}', 'UsersController@destroy')->name('users.admin.delete');
+    Route::get('/userAdminSearch', 'UsersController@search')->name('users.admin.search');
+
+   
 });
